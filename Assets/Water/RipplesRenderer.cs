5ad31df7e16e9d84e384ecf996f498f6bad563ce
Jordi <jcaballol94@gmail.com>
2021-05-21 20:22:26 +0100

    Setup the ripples renderer

--- Assets/Water/RipplesRenderer.cs
+++ Assets/Water/RipplesRenderer.cs
@@ -13,7 +13,7 @@ public class RipplesRenderer : MonoBehaviour
     private Hashtable m_rippleCameras = new Hashtable(); // Camera -> Camera table 
 
     private RenderTexture m_ripplesTexture = null;
-    private int m_OldRippleTextureSize = 0;
+    private Vector2Int m_OldRippleTextureSize = Vector2Int.zero;
 
     private static bool s_InsideRendering = false;
 
@@ -120,24 +120,24 @@ public class RipplesRenderer : MonoBehaviour
         reflectionCamera = null;
 
         // Reflection render texture
-        if (!m_ripplesTexture || m_OldRippleTextureSize != Screen.currentResolution.height)
+        if (!m_ripplesTexture || m_OldRippleTextureSize.x != Screen.currentResolution.width || m_OldRippleTextureSize.y != Screen.currentResolution.height)
         {
             if (m_ripplesTexture)
                 DestroyImmediate(m_ripplesTexture);
-            m_ripplesTexture = new RenderTexture(m_TextureSize, m_TextureSize, 16);
+            m_ripplesTexture = new RenderTexture(Screen.currentResolution.width, Screen.currentResolution.height, 16);
             m_ripplesTexture.useMipMap = true;
             m_ripplesTexture.autoGenerateMips = true;
-            m_ripplesTexture.name = "__MirrorReflection" + GetInstanceID();
+            m_ripplesTexture.name = "__RipplesTexture" + GetInstanceID();
             m_ripplesTexture.isPowerOfTwo = true;
             m_ripplesTexture.hideFlags = HideFlags.DontSave;
-            m_OldRippleTextureSize = m_TextureSize;
+            m_OldRippleTextureSize = new Vector2Int(Screen.currentResolution.width, Screen.currentResolution.height);
         }
 
         // |Camera for reflection
         reflectionCamera = m_rippleCameras[currentCamera] as Camera;
         if (!reflectionCamera) // catch both not-in-dictionary and in-dictionary-but-deleted-GO
         {
-            GameObject go = new GameObject("Mirror Refl Camera id" + GetInstanceID() + " for " + currentCamera.GetInstanceID(), typeof(Camera));
+            GameObject go = new GameObject("Ripples Camera id" + GetInstanceID() + " for " + currentCamera.GetInstanceID(), typeof(Camera));
             reflectionCamera = go.GetComponent<Camera>();
             reflectionCamera.enabled = false;
             reflectionCamera.transform.position = transform.position;
@@ -145,65 +145,5 @@ public class RipplesRenderer : MonoBehaviour
             go.hideFlags = HideFlags.HideAndDontSave;
             m_rippleCameras[currentCamera] = reflectionCamera;
         }
-    }
-
-    // 0 or 1（Extended sign: returns -1, 0 or 1 based on sign of a）
-    private static float sgn(float a)
-    {
-        if (a > 0.0f) return 1.0f;
-        if (a < 0.0f) return -1.0f;
-        return 0.0f;
-    }
-
-    //（Given position/normal of the plane, calculates plane in camera space.）
-    private Vector4 CameraSpacePlane(Camera cam, Vector3 pos, Vector3 normal, float sideSign)
-    {
-        Vector3 offsetPos = pos + normal * m_ClipPlaneOffset;
-        Matrix4x4 m = cam.worldToCameraMatrix;
-        Vector3 cpos = m.MultiplyPoint(offsetPos);
-        Vector3 cnormal = m.MultiplyVector(normal).normalized * sideSign;
-        return new Vector4(cnormal.x, cnormal.y, cnormal.z, -Vector3.Dot(cpos, cnormal));
-    }
-
-    // Adjusts the given projection matrix so that near plane is the given clipPlane）
-    // clipPlane is given in camera space. See article in Game Programming Gems 5.）
-    private static void CalculateObliqueMatrix(ref Matrix4x4 projection, Vector4 clipPlane)
-    {
-        Vector4 q = projection.inverse * new Vector4
-        (sgn(clipPlane.x),
-            sgn(clipPlane.y),
-            1.0f,
-            1.0f);
-
-        Vector4 c = clipPlane * (2.0F / (Vector4.Dot(clipPlane, q)));
-        // third row = clip plane - fourth row）
-        projection[2] = c.x - projection[3];
-        projection[6] = c.y - projection[7];
-        projection[10] = c.z - projection[11];
-        projection[14] = c.w - projection[15];
-    }
-
-    // Calculates reflection matrix around the given plane）
-    private static void CalculateReflectionMatrix(out Matrix4x4 reflectionMat, Vector4 plane)
-    {
-        reflectionMat.m00 = (1F - 2F * plane[0] * plane[0]);
-        reflectionMat.m01 = (-2F * plane[0] * plane[1]);
-        reflectionMat.m02 = (-2F * plane[0] * plane[2]);
-        reflectionMat.m03 = (-2F * plane[3] * plane[0]);
-
-        reflectionMat.m10 = (-2F * plane[1] * plane[0]);
-        reflectionMat.m11 = (1F - 2F * plane[1] * plane[1]);
-        reflectionMat.m12 = (-2F * plane[1] * plane[2]);
-        reflectionMat.m13 = (-2F * plane[3] * plane[1]);
-
-        reflectionMat.m20 = (-2F * plane[2] * plane[0]);
-        reflectionMat.m21 = (-2F * plane[2] * plane[1]);
-        reflectionMat.m22 = (1F - 2F * plane[2] * plane[2]);
-        reflectionMat.m23 = (-2F * plane[3] * plane[2]);
-
-        reflectionMat.m30 = 0F;
-        reflectionMat.m31 = 0F;
-        reflectionMat.m32 = 0F;
-        reflectionMat.m33 = 1F;
     }
 }
